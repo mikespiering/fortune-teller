628d3997310c6a0418cd920547c466ebed982376
Cora Iberkleid <ciberkleid@pivotal.io>
2019-06-17 13:41:59 -0400

    removed cb-dashboard

--- scripts/undeploy.sh
+++ scripts/undeploy.sh
@@ -8,7 +8,6 @@ cf delete fortune-ui -f
 cf delete-service fortunes-db -f
 cf delete-service fortunes-config-server -f
 cf delete-service fortunes-service-registry -f
-cf delete-service fortunes-circuit-breaker-dashboard -f
 cf delete-service fortunes-cloud-bus -f
 
 # delete routes
