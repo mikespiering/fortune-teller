b188120007df62c70c03e298aad05b4b99e52b4a
Cora Iberkleid <ciberkleid@pivotal.io>
2019-06-17 13:35:40 -0400

    removed cb-dashboard

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,6 @@ if [[ $CF_API == *"api.run.pivotal.io"* ]]; then
     cf create-service cleardb spark fortunes-db
     cf create-service p-config-server trial fortunes-config-server -c '{"git": { "uri": "https://github.com/ciberkleid/fortune-teller", "searchPaths": "configuration" } }'
     cf create-service p-service-registry trial fortunes-service-registry
-    cf create-service p-circuit-breaker-dashboard trial fortunes-circuit-breaker-dashboard
     cf create-service cloudamqp lemur fortunes-cloud-bus
 else
     if [ ! -z "`cf m | grep "p\.config-server"`" ]; then
@@ -27,7 +26,6 @@ else
     cf cs p.mysql db-small fortunes-db
     cf cs $service_name standard fortunes-config-server -c "$config_json"
     cf cs p-service-registry standard fortunes-service-registry
-    cf cs p-circuit-breaker-dashboard standard fortunes-circuit-breaker-dashboard
     cf create-service p.rabbitmq single-node-3.7 fortunes-cloud-bus
 fi
 
